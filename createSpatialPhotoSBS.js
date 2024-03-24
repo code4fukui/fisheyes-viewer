1c11eafc74621318ecd03f8f00e68c95e92c9516
Taisuke Fukuno <fukuno@jig.jp>
2024-03-24 09:31:36 +0900

    add debug

--- createSpatialPhotoSBS.js
+++ createSpatialPhotoSBS.js
@@ -2,16 +2,32 @@ import * as THREE from "three";
 
 // 2048x1366
 // 1024x1366
+//const debug = true;
+const debug = false;
 
 export const createSpatialPhotoSBS = (img, reverse) => {
-  const photo = new THREE.Group();
-  const texture = new THREE.TextureLoader().load(img);
+  const scale = debug ? 1 : 2;
+  const wireframe = debug;
+
+  const texture = debug ? null : new THREE.TextureLoader().load(img);
   const left = reverse ? 2 : 1;
-  const scale = 2;
+
+  const scalex = 2.1;
+  const scaley = scale * 1.366 / 1.024;
+
+  const photo = new THREE.Group();
   for (let i = 1; i <= 2; i++) {
-    const geometry = new THREE.PlaneGeometry(1.024 * scale, 1.366 * scale); // for Canon
+    //const geometry = new THREE.PlaneGeometry(1.024 * scale, 1.366 * scale); // for Canon
+    // radius, widthseg, heightseg, phiStart, phiLength, thetaStart, thetaLength
+    const geometry = new THREE.SphereGeometry(scale / 2, 36, 12, 0, Math.PI * 2, 0, Math.PI / 2);
     // adjust texture
     const uvs = geometry.attributes.uv.array;
+    const pos = geometry.attributes.position.array;
+    for (let j = 0; j < uvs.length / 2; j++) {
+        uvs[j * 2] = pos[j * 3 + 0] / scalex + 0.5 + (i == 1 ? 0.02 : -0.02);
+        uvs[j * 2 + 1] = pos[j * 3 + 2] / scaley + 0.5;
+    }
+    
     if (i == left) {
       for (let j = 0; j < uvs.length; j += 2) {
           uvs[j] *= 0.5;
@@ -21,12 +37,22 @@ export const createSpatialPhotoSBS = (img, reverse) => {
           uvs[j] = uvs[j] * 0.5 + 0.5;
       }
     }
-    const material = new THREE.MeshBasicMaterial({ map: texture });
+    
+    const material = new THREE.MeshBasicMaterial({
+      map: texture,
+      side: THREE.BackSide,
+      wireframe,
+    });
     //const material = new THREE.MeshToonMaterial({ color: 0x6699FF }) 
     const plane = new THREE.Mesh(geometry, material);
 
     // planeの配置
     plane.layers.set(i); // display in left/right eye only
+    //plane.layers.set(i == 1 ? 2 : 1); // display in left/right eye only
+    //plane.rotation.z = Math.PI / 2;
+    plane.rotation.x = Math.PI / 2;
+    plane.rotation.y = Math.PI;
+    plane.rotation.z = Math.PI;
     plane.position.x = 0;
     plane.position.y = 0;
     plane.position.z = -1;
